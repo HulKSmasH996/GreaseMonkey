7ad530eeeca41043f8a2297dfb79fb613fcb15d2
Subhojeet Sen <1421036@gmail.com>
2017-11-26 09:37:02 +0530

    Auto-Detect Location(Crude)

--- app/src/main/java/com/kiit/viper/greasemonkey/MainActivity.java
+++ app/src/main/java/com/kiit/viper/greasemonkey/MainActivity.java
@@ -1,13 +1,149 @@
 package com.kiit.viper.greasemonkey;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION = 1;
+    TextView textView;
+    ProgressBar progressBar;
+    LocationManager locationManager;
+    //String latitude,longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        textView = (TextView)findViewById(R.id.text_location);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        textView.setText("");
+        /**/
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            try {
+                getLocation();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
+
+    private void getLocation() throws IOException {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            Location location2 = locationManager.getLastKnownLocation(LocationManager. PASSIVE_PROVIDER);
+
+            if (location != null) {
+                double lati = location.getLatitude();
+                double longi = location.getLongitude();
+                //latitude = String.valueOf(lati);
+                //longitude = String.valueOf(longi);
+
+
+                textView.setText(getAddress(lati,longi));
+                 progressBar.setVisibility(View.GONE);
+
+            } else  if (location1 != null) {
+                double lati = location1.getLatitude();
+                double longi = location1.getLongitude();
+                //latitude = String.valueOf(lati);
+                //longitude = String.valueOf(longi);
+
+                textView.setText(getAddress(lati,longi));
+                progressBar.setVisibility(View.GONE);
+
+
+            } else  if (location2 != null) {
+                double lati = location2.getLatitude();
+                double longi = location2.getLongitude();
+               // latitude = String.valueOf(lati);
+                //longitude = String.valueOf(longi);
+
+                textView.setText(getAddress(lati,longi));
+                progressBar.setVisibility(View.GONE);
+
+            }else{
+
+                Toast.makeText(this,"Unble to Trace your location",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    protected void buildAlertMessageNoGps() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Please Turn ON your GPS Connection")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+    String getAddress(double lat,double longi) throws IOException {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        addresses = geocoder.getFromLocation(lat, longi, 1);
+
+        String address = addresses.get(0).getAddressLine(0);
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String country = addresses.get(0).getCountryName();
+        String postalCode = addresses.get(0).getPostalCode();
+        String knownName = addresses.get(0).getFeatureName();
+
+        return address+"\n"+city+"\n"+state+"\n"+country+"\n"+postalCode+"\n"+knownName;
+    }
+
+
 }
